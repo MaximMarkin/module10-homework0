eb75ac2ebf5852cf6377a379c9a15ad64fad37ea
7cod <125095130+MaximMarkin@users.noreply.github.com>
2024-01-10 20:55:48 +0300

    Add files via upload

---
+++ practice  №6.js
@@ -0,0 +1,17 @@
+
+
+let array = ["n", "n", "n", "n", "n", "n"];
+
+let same = true;
+
+for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] !== array[i + 1]) {
+        same = false;
+        break;
+    }
+}
+if (same) {
+    console.log("true");
+} else {
+    console.log("false");
+}
